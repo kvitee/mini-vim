c4f017feaff9fb09d3d828b30812f81c21e5246f
kvitee <kvitee41@gmail.com>
2026-09-20 12:38:44 +0800

    Update setup script.
    Change VimPlug installation directory to ~/.local/share/vim;
    Fix comments erasing.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.pre.vimrc
 
 # Install Vim-Plug
-curl -fsSLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fsSLo ~/.local/share/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Copy .vimrc and remove all comments and empty lines
-curl -fsSL https://raw.githubusercontent.com/kvitee/dots/main/core/dot-vimrc | sed 's/ *".*$//;/^$/d' > ~/.vimrc
+curl -fsSL https://raw.githubusercontent.com/kvitee/dots/main/core/dot-vimrc | sed 's/^".*$//;/^$/d' > ~/.vimrc
 
 # Install plugins
 yes | vim +PlugInstall +qa 2> /dev/null
